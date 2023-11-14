7117a5c63c7170dd790173f2def201a2212f8731
jjustin08 <justin_noah17@yahoo.ca>
2023-11-13 21:51:34 -0500

    ADD seated cable row prototype

---
+++ Gym Sim/Assets/Scripts/Machines/Machines/Row.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Row : BaseMachine
+{
+    [SerializeField] private Slider slider;
+
+    [SerializeField] private Transform character;
+
+    private bool isReseting;
+
+    private float pullAmount = 0.1f;
+    private float resetSpeed = 0.001f;
+    private void Update()
+    {
+        if(isActive)
+        {
+            if(Input.GetKeyDown(KeyCode.Space)) 
+            {
+                isRunning = true;
+            }
+            if (isRunning)
+            {
+                Controls();
+                MachineUpdate();
+            }
+        }
+        
+    }
+    private void Controls()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            slider.value += pullAmount;
+        }
+    }
+
+    private void MachineUpdate()
+    {
+        Vector3 newPos = character.position;
+        newPos.x = -slider.value * 2;
+        newPos.z = 0;
+        character.localPosition = newPos;
+
+        slider.value -= resetSpeed * (slider.value * 2);
+
+        if (slider.value >= 0.70f)
+        {
+            isReseting = true;
+        }
+        if (isReseting)
+        {
+            if (slider.value <= 0)
+            {
+                isReseting = false;
+            }
+            slider.value -= resetSpeed;
+        }
+    }
+}
