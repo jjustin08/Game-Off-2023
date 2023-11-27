2f735a8ce7b7a94c3a9a9a164d7a1bb0ef5b5752
jjustin08 <justin_noah17@yahoo.ca>
2023-11-26 23:19:15 -0500

    ADD new bicep slider game

---
+++ Gym Sim/Assets/Scripts/Machines/Utill/SliderGameCurved.cs
@@ -0,0 +1,175 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderGameCurved : MonoBehaviour
+{
+    [SerializeField] private Image targetMax;
+    [SerializeField] private Image targetMin;
+
+    [SerializeField] private RectTransform pointer;
+    [SerializeField] private Transform pointerPin;
+
+    [SerializeField] private bool isLeft;
+
+    private float targetSize = 30f;
+
+    float currentRotation = 90.0f;
+
+    float rotationSpeed = 0.5f;
+
+    float minRotation = -90.0f;
+    float maxRotation = 90.0f;
+
+    private bool isActive = false;
+
+    //TODO add a activation function
+    private void Update()
+    {
+        if (!isActive && gameObject.active)
+        {
+            isActive = true;
+
+            ChangeTargetLocation(targetSize);
+        }
+        else if(isActive && !gameObject.active)
+        {
+            isActive = false;
+        }
+
+
+        if(isActive)
+        {
+            Controls();
+            GameUpdate();
+        }
+
+    }
+
+    private void GameUpdate()
+    {
+        // Calculate the new rotation angle based on the current angle and direction
+        float newRotation = currentRotation + rotationSpeed;
+
+        // Check if the new rotation angle exceeds the limits (-45 to 45 degrees)
+        if (newRotation > maxRotation || newRotation < minRotation)
+        {
+            // Reverse the rotation direction
+            rotationSpeed *= -1;
+        }
+
+        // Update the current rotation angle
+        currentRotation += rotationSpeed;
+
+        // Apply the rotation
+        pointer.RotateAround(pointerPin.position, new Vector3(0, 0, 1), rotationSpeed);
+
+        
+    }
+
+
+    private void Controls()
+    {
+        if (isLeft)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                CheckIfInside();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CheckIfInside();
+            }
+        }
+
+    }
+
+
+
+    private void CheckIfInside()
+    {
+        float progress = CalculateRotationPercentage();
+
+        float max = targetMax.fillAmount * 100;
+        float min = targetMin.fillAmount * 100;
+
+        print("min: " + min);
+        print("roation: " + progress);
+        print("max: " + max);
+
+
+        if (progress >= min && progress <= max)
+        {
+            HitTarget();
+        }
+        else
+        {
+            //MissTarget();
+        }
+    }
+
+    private float CalculateRotationPercentage()
+    {
+        float mappedRotation = currentRotation - minRotation;
+
+        float percentage = (mappedRotation / (maxRotation - minRotation)) * 100.0f;
+
+        return percentage;
+    }
+
+    private void HitTarget()
+    {
+        
+        // decreasing size of target
+        if ((targetSize = targetSize / 1.2f) >= 1)
+        {
+            ChangeTargetLocation(targetSize);
+        }
+        else
+        {
+            //game over??
+        }
+
+
+    }
+
+    //private void MissTarget()
+    //{
+    //    if ((targetSize = targetSize / 1.2f) >= 5)
+    //    {
+    //        ChangeTargetLocation(targetSize);
+    //    }
+    //    else
+    //    {
+    //        //game over??
+    //    }
+    //}
+
+
+
+
+
+
+
+    void ChangeTargetLocation(float size)
+    {
+        float randomRotation = 0;
+        if (targetMin.fillAmount >= 0.5)
+        {
+            randomRotation = UnityEngine.Random.Range(20, 50);
+        }
+        else
+        {
+            randomRotation = UnityEngine.Random.Range(50, 80);
+        }
+
+        randomRotation /= 100;
+
+        targetMin.fillAmount = randomRotation;
+        targetMax.fillAmount = randomRotation + (size/ 100);
+    }
+}
